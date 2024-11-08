f818f4e3b013cce475ab58d3d7574f5dc4ca737a
anonymouslearning685 <learninga685@gmail.com>
2024-11-08 13:57:36 +0700

    version:0.0.1 msg: "Create app.py"

---
+++ app.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+city = input("Which city did you grow in? ")
+print(city)
+
+pet = input("What is the name of a pet? ")
+print("Your band name could be: " + city + " " + pet)
